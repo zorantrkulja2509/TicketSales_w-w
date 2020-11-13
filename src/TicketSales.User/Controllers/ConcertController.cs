110570f5d958cb7137826d3c66e0e3d71fc36410
Zoran Trkulja <zoran.trkulja@elanwave.com>
2020-11-13 08:49:32 +0100

    Added ignored files

---
+++ src/TicketSales.User/Controllers/ConcertController.cs
@@ -0,0 +1,45 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using TicketSales.Messages.Commands;
+using TicketSales.Messages.Events;
+
+namespace TicketSales.User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConcertController : Controller
+    {
+        public readonly IServiceProvider _serviceProvider;
+        private readonly IRequestClient<GetAllConcertsCommand> _requestClient;
+        private readonly IRequestClient<BuyTicketCommand> _requestClientBuyTicketCommand;
+
+        public ConcertController(IServiceProvider serviceProvider,
+                                 IRequestClient<GetAllConcertsCommand> requestClient,
+                                 IRequestClient<BuyTicketCommand> requestClientBuyTicketCommand)
+        {
+            _serviceProvider = serviceProvider;
+            _requestClient = requestClient;
+            _requestClientBuyTicketCommand = requestClientBuyTicketCommand;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var request = _requestClient.Create(new GetAllConcertsCommand());
+            var response = await request.GetResponse<GetAllConcertsEvent>();
+
+            return View(response.Message.ConcertViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuyTickets([FromForm] BuyTicketCommand buyTicketCommand)
+        {
+            var request = _requestClientBuyTicketCommand.Create(buyTicketCommand);
+            await request.GetResponse<BuyTicketsEvent>();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/TicketSales.User/Controllers/TicketsController.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using TicketSales.Messages.Commands;
+using TicketSales.Messages.Events;
+
+namespace TicketSales.User.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : Controller
+    {
+        public readonly IServiceProvider _serviceProvider;
+        private readonly IRequestClient<GetTicketByUserIdCommand> _requestClient;
+
+        public TicketsController(IServiceProvider serviceProvider,
+                                 IRequestClient<GetTicketByUserIdCommand> requestClient)
+        {
+            _serviceProvider = serviceProvider;
+            _requestClient = requestClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var request = _requestClient.Create(new GetTicketByUserIdCommand() { UserId = 2 });
+            var response = await request.GetResponse<GetTicketByUserIdEvent>();
+
+            return View(response.Message.TicketViewModel);
+        }
+    }
+}
